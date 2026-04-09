2be1b382f5f5babc7207802f07dfd238e658776c
Sanghel González <sanghelgonzalez@gmail.com>
2026-04-09 12:44:12 -0400

    feat: Render all 8 planets in the 3D scene by mapping planet data (closes #25)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { SolarSystemCanvas } from './components/Scene/SolarSystemCanvas';
 import { Lights } from './components/Scene/Lights';
 import { Sun } from './components/Scene/Sun';
+import { Planet } from './components/Scene/Planet';
+import { planets } from './data/planets';
 
 function App() {
+  // Filter out the Sun, render only planets
+  const planetsToRender = planets.filter((p) => p.type !== 'star');
+
   return (
     <SolarSystemCanvas>
       <Lights />
       <Sun />
+      {planetsToRender.map((planet) => (
+        <Planet key={planet.id} planet={planet} />
+      ))}
     </SolarSystemCanvas>
   );
 }
