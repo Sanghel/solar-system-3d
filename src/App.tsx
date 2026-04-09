1d4825ace777981857675943b408fc377bdce381
Sanghel González <sanghelgonzalez@gmail.com>
2026-04-09 13:12:08 -0400

    feat: Add onClick event handler to Planet component with selection feedback (closes #32)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { Lights } from './components/Scene/Lights';
 import { Sun } from './components/Scene/Sun';
 import { Planet } from './components/Scene/Planet';
 import { planets } from './data/planets';
+import { usePlanetSelection } from './hooks/usePlanetSelection';
 
 function App() {
+  const { selectedPlanet, selectPlanet } = usePlanetSelection();
+
   // Filter out the Sun, render only planets
   const planetsToRender = planets.filter((p) => p.type !== 'star');
 
@@ -13,7 +16,13 @@ function App() {
       <Lights />
       <Sun />
       {planetsToRender.map((planet, index) => (
-        <Planet key={planet.id} planet={planet} index={index} />
+        <Planet
+          key={planet.id}
+          planet={planet}
+          index={index}
+          onSelect={selectPlanet}
+          isSelected={selectedPlanet?.id === planet.id}
+        />
       ))}
     </SolarSystemCanvas>
   );
